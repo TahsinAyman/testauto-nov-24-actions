43a37e10ce9ca18f1ce314a949455a7c91bf48ff
fullstackltd <fullstackltd@gmail.com>
2024-03-05 20:01:37 +0600

    To Deploy

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+// javac Main.java -> Build / Compilation
+// java Main -> Deploy / Execution
